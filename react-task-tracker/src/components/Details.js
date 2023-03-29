16217498cbdbef7decd0794fb3b6a02209da20e2
Manuel La Porta <90390753+Manulapo@users.noreply.github.com>
2023-03-30 01:06:37 +0200

    final --v

---
+++ react-task-tracker/src/components/Details.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Details = () => {
+    return ( 
+        <div className="">
+            Hello!
+            <div><Link to="/">Go Back</Link></div>
+        </div>
+    );
+}
+ 
+export default Details;
